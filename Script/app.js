b583c2b7dfb4b9f68d8988eec43296ea32df6c62
AkashdeepSIN <azzy.virk.13@gmail.com>
2016-03-05 17:15:55 -0500

    first commit

---
+++ Script/app.js
@@ -0,0 +1,14 @@
+"use strict";
+
+// setup your IIFE(Immediately Invoked Function Ewperession) hint
+(function(){
+	
+	
+		var firstParagraph;
+
+		firstParagraph = document.getElementById("me");
+
+		firstParagraph.innerHTML = "Welcome to my resume, My Name is Akashdeep Singh. I am pursuing my graduation in Georgian College in Interactive Web designing. My aim is to become a web producer. I making alot of projects in my studying time. The period of my studying in Georgian college is really amazing. I always try to learn as more I can. I want to make my website, so it will be useful for needy people. ";
+	
+	
+})();
